117e3c1f7afff9701def290f679ed77f2c8ad528
Horáček, Michal <horacmi8@fel.cvut.cz>
2023-01-21 14:50:53 +0100

    MORE DATA

--- code/c++onvert2bin/json2txt.py
+++ code/c++onvert2bin/json2txt.py
@@ -42,16 +42,79 @@ def read_sportka():
     )
     
     return csv.to_numpy().flatten()
-    
 
-lotto_numbers = read_lotto()
-eurojackpot_numbers = read_eurojackpot()
-sportka_numbers = read_sportka()
+def read_ny_lotto():
+    columns = ["Date", "Draw", "Bonus", "Extra"]
+
+    csv = pd.read_csv(
+        "data/NY_Lotto.csv",
+        sep=',',
+        header=0,
+        names=columns,
+        usecols=["Draw"]
+    )
+
+    numbers = [
+        [int(n) for n in row.split()] for row in csv["Draw"]
+    ]
+
+    return reduce(lambda a, b: a + b, numbers)
+
+def read_uk_lotto():
+    time_info = ["No.", "Day", "DD", "MMM", "YYYY"]
+    draw = ["Number 1", "Number 2", "Number 3", "Number 4", "Number 5", "Number 6", "Bonus Number"]
+    win_info = ["Jackpot", "Wins", "Machine", "Set"]
+
+    csv = pd.read_csv(
+        "data/UK_Lotto_drawn.csv",
+        sep=',',
+        header=0,
+        names=time_info + draw + win_info,
+        usecols=draw
+    )
+
+    return csv.to_numpy().flatten()
+
+def read_uk_lotto_tuesday():
+    time_info = ["No.", "Day", "DD", "MMM", "YYYY"]
+    draw = ["Number 1", "Number 2", "Number 3", "Number 4", "Number 5", "Number 6"]
+    win_info = ["Jackpot", "Wins", "Machine", "Set"]
+
+    csv = pd.read_csv(
+        "data/UK_Lotto_tuesdays_drawn.csv",
+        sep=',',
+        header=0,
+        names=time_info + draw + win_info,
+        usecols=draw
+    )
+
+    return csv.to_numpy().flatten()
+
+def read_texas_lotto():
+    time_info = ["Game Name", "Month", "Day", "Year"]
+    draw = ["Number 1", "Number 2", "Number 3", "Number 4", "Number 5", "Number 6"]
+
+    csv = pd.read_csv(
+        "data/Texas_Lotto.csv",
+        sep=',',
+        header=0,
+        names=time_info + draw,
+        usecols=draw
+    )
+
+    return csv.to_numpy().flatten()
+
+readers = [
+    read_lotto,
+    read_eurojackpot,
+    read_sportka,
+    read_ny_lotto,
+    read_uk_lotto,
+    read_uk_lotto_tuesday,
+    read_texas_lotto
+]
 
 with open("data/lottery_numbers.txt", "w") as out:
-    for number in lotto_numbers:
-        print(number, file=out)
-    for number in eurojackpot_numbers:
-        print(number, file=out)
-    for number in sportka_numbers:
-        print(number, file=out)
+    for read in readers:
+        for number in read():
+            print(number, file=out)
