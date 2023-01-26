1d1296a2e8b5636741a1cfc7c047fb0a56f45350
Horáček, Michal <horacmi8@fel.cvut.cz>
2023-01-26 21:44:11 +0100

    Fix uniformity issue

--- code/c++onvert2bin/txt2bin.cpp
+++ code/c++onvert2bin/txt2bin.cpp
@@ -15,17 +15,19 @@ int main(int argc, char **argv) {
         return 1;
     }
 
+    constexpr uint32_t max_power = 64;  // highest power of 2 s.t. <= highest possible lottery number
     uint32_t random_number;
     std::vector<uint32_t> numbers;
     while(source_file >> random_number) {
-        uint32_t sanitized = (random_number - 1) & 0b11111;
-        numbers.push_back(sanitized);
+        if(random_number > max_power) continue;
+
+        numbers.push_back(random_number - 1);
     }
     std::cout << "Loaded " << numbers.size() << " numbers from source file " << argv[1] << ".\n";
 
     source_file.close();
 
-    std::ofstream output_file{argv[2], std::ios::out | std::ios::binary | std::ios::trunc};
+    std::ofstream output_file{argv[2], std::ios::out | std::ios::binary | std::ios::app};
     if(!output_file.good()) {
         std::cout << "Error in opening the output file " << argv[2] << "\n";
         return 1;
@@ -35,7 +37,7 @@ int main(int argc, char **argv) {
     uint32_t buffer = 0;
     for(uint8_t number : numbers) {
         buffer |= number << valid_bits;
-        valid_bits += 5;
+        valid_bits += 6;
 
         if(valid_bits >= 8) {
             uint8_t byte = buffer & 0xFF;
